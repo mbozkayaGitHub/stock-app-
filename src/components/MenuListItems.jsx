8ea19d9e585201a542c8009cd3ff8c766d84f1c8
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-03-28 21:41:20 +0300

    scgIcon

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -1,12 +1,10 @@
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
+
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from "@mui/icons-material/Menu"
 import DashboardIcon from "@mui/icons-material/Dashboard"
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney"
 import InventoryIcon from "@mui/icons-material/Inventory"
@@ -14,24 +12,67 @@ import StoreIcon from "@mui/icons-material/Store"
 import StarsIcon from "@mui/icons-material/Stars"
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount"
+import { useNavigate } from 'react-router-dom';
 
-const icons = [{
-    title: "Dashboard",
-    icon:<MailIcon/>,
-    url:"/stock",
-}]
+const icons = [
+    {
+      icon: <DashboardIcon />,
+      title: "Dashboard",
+      url: "/stock/",
+    },
+    {
+      title: "Purchase",
+      icon: <ShoppingCartIcon />,
+      url: "/stock/purchases/",
+    },
+    {
+      title: "Sales",
+      icon: <AttachMoneyIcon />,
+      url: "/stock/sales/",
+    },
+    {
+      title: "Firms",
+      icon: <StoreIcon />,
+      url: "/stock/firms/",
+    },
+    {
+      title: "Brands",
+      icon: <StarsIcon />,
+      url: "/stock/brands/",
+    },
+    {
+      title: "Products",
+      icon: <InventoryIcon />,
+      url: "/stock/products/",
+    },
+    {
+      title: "Admin Panel",
+      icon: <SupervisorAccountIcon />,
+      url: "https://12256.fullstack.clarusway.com/",
+    },
+  ]
+  
 
 const MenuListItems = () => {
+    const iconStyle = {
+        color:"white", 
+        "& .MuiSvgIcon-root": {color:"white"},
+        "&:hover":{color:"red"},
+        "&:hover .MuiSvgIcon-root" : {color:"red"},
+
+    }
+
+      const navigate = useNavigate()
   return (
     <div>
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              
+        {icons?.map((item, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton onClick={()=> navigate(item.url)} sx={iconStyle}>
+              <ListItemIcon >
+              {item.icon}
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={item.title} sx={iconStyle} />
             </ListItemButton>
           </ListItem>
         ))}
