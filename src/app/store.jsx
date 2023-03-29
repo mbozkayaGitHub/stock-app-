a2540bfbcf4b8b8815c295e2684a0502cf42b2dc
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-03-29 06:49:11 +0300

    store.

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,5 +1,6 @@
-import { configureStore, createStore } from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import stockReducer from "../features/stockSlice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
@@ -13,7 +14,8 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: persistReducer,
+    auth: persistedReducer,
+    stock:stockReducer
   },
   devTools: process.env.NODE_ENV !== "production",
 });
