7858eb9629a8672077a0906e3542fd43f60cf67f
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-04-07 23:18:42 +0300

    Firm

---
+++ src/components/modals/FirmModal.jsx
@@ -0,0 +1,91 @@
+import React, {useState} from 'react';
+import Box from '@mui/material/Box';
+
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import {modalStyle} from "../../styles/globalStyle" 
+import { Button, TextField } from '@mui/material';
+
+
+export default function FirmModal({open,handleClose}) {
+
+  const [info,setInfo] =  useState({
+    name:"",
+    phone:"",
+    address:"",
+    image:"",
+  }) 
+  const handleChange = (e) => {
+    const {name,value} = e.target
+    setInfo({...info, [name]:value})
+  }
+  const handleSubmit = () => {
+    
+  }
+  console.log(info);
+  return (
+    <div>
+      
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+        <TextField
+          label="Firm Name"
+          name="name"
+          id="name"
+          type="text"
+          variant="outlined"
+          required
+          value={info?.name}
+          onChange={handleChange}
+         
+        />
+        <TextField
+          label="Phone"
+          name="phone"
+          id="phone"
+          type="tel"
+          variant="outlined"
+          required
+          value={info?.phone}
+          onChange={handleChange}
+         
+        />
+       
+        <TextField
+          label="Address"
+          name="address"
+          id="address"
+          type="tel"
+          variant="outlined"
+          required
+          value={info?.address}
+          onChange={handleChange}
+         
+        />
+        <TextField
+          label="image"
+          name="image"
+          id="image"
+          type="url"
+          variant="outlined"
+          required
+          value={info?.image}
+          onChange={handleChange}
+         
+        />
+        <Button  type="submit"variant="contained"
+        onSubmit={handleSubmit}
+        >Submit</Button>
+
+        </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
