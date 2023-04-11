927b58875d1e37aa7a8b41cc7b0872a1a4b61a1f
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-04-11 06:22:15 +0300

    Products

---
+++ src/components/modals/ProductModal.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { modalStyle } from "../../styles/globalStyle";
+import { Button, TextField } from "@mui/material";
+import useStockCall from "../../hooks/useStockCall";
+
+export default function FirmModal({ open, handleClose,info,setInfo }) {
+  // const [info, setInfo] = useState({
+  //   name: "",
+  //   phone: "",
+  //   address: "",
+  //   image: "",
+  // });
+  const { postStockData } = useStockCall();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInfo({ ...info, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postStockData("firms", info);
+    handleClose();
+    setInfo({  name: "",
+    phone: "",
+    address: "",
+    image: "",})
+  };
+  console.log(info);
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={()=>{
+          handleClose()
+          setInfo({  name: "",
+          phone: "",
+          address: "",
+          image: "",})
+        }}
+        
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          <Box
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            component="form"
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              label="Firm Name"
+              name="name"
+              id="name"
+              type="text"
+              variant="outlined"
+              required
+              value={info?.name}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Phone"
+              name="phone"
+              id="phone"
+              type="tel"
+              variant="outlined"
+              required
+              value={info?.phone}
+              onChange={handleChange}
+            />
+
+            <TextField
+              label="Address"
+              name="address"
+              id="address"
+              type="tel"
+              variant="outlined"
+              required
+              value={info?.address}
+              onChange={handleChange}
+            />
+            <TextField
+              label="image"
+              name="image"
+              id="image"
+              type="url"
+              variant="outlined"
+              required
+              value={info?.image}
+              onChange={handleChange}
+            />
+            <Button type="submit" variant="contained">
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,53 @@
+import { Button, Grid, Typography } from "@mui/material";
+import React, { useEffect,useState } from "react";
+import useStockCall from "../hooks/useStockCall";
+import { useSelector } from "react-redux";
+import FirmCard from "../components/FirmCard";
+import { flex } from "../styles/globalStyle";
+import FirmModal from "../components/modals/FirmModal";
 
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
 
-export default Products
+   const {getStockData} = useStockCall()
+   const {firms} = useSelector((state)=> state.stock)
+   const [open, setOpen] =useState(false);
+     const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    image: "",
+  });
+   const handleOpen = () => setOpen(true);
+   const handleClose = () => setOpen(false);
+ 
+  useEffect(() => {
+
+
+    getStockData("firms")
+  }, []);
+
+      console.log(firms);
+
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Firm
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>New Firm</Button>
+     
+     <FirmModal open={open} handleClose={handleClose} info={info} setInfo={info}/>
+        <Grid container sx={flex}>
+        {firms?.map((firm)=> (
+          <Grid item key={firm.id}>
+            <FirmCard firm={firm} setOpen={setOpen} setInfo={setInfo} />
+
+          </Grid>
+            ))}
+        </Grid>
+    
+    </div>
+  );
+};
+
+export default Products;
